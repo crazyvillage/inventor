b023e8cace4545459e36b1a6604c2dae725aa0ee
crazyvillage <mag@crazyvillage.net>
2024-02-16 06:16:19 -0800

    fix: warnings

--- Inventor/SeedWorks/Summary.cs
+++ Inventor/SeedWorks/Summary.cs
@@ -3,6 +3,6 @@ namespace CrazyVillage.Inventor.SeedWorks;
 public class Summary
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
 }

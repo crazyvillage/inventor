2f229a27e1c47111d28d49b4e453d94b6bf5d891
crazyvillage <mag@crazyvillage.net>
2024-02-16 05:12:30 -0800

    feat: redoc

--- Inventor/Program.cs
+++ Inventor/Program.cs
@@ -1,8 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseOpenApi();
+app.UseReDoc(options =>
+{
+    options.Path = "/redoc";
+});
 
 app.UseHttpsRedirection();
 
